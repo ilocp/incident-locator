7ef990f2403188a11ac3c46c216cec1dce499ffa
Tasos Latsas <tlatsas2000@gmail.com>
2012-08-09 18:18:05 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IncidentLocator::Application.config.secret_token = '0d09e4c8cfc6973ffb2e14c895fded943b02db7150bd4b5fde8cbb80a952bc77fe95f6faa3cb58a6d3247db20f38c0961989c0bc2d83c428b25e19fb84f01e36'
