c2192f653960a832348d113ac08308acf5e3a5f9
Tasos Latsas <tlatsas2000@gmail.com>
2012-08-24 16:14:40 +0300

    Add functionality for Login/Logout and Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if user = User.authenticate(params[:email], params[:password])
+      sign_in user
+      redirect_to user
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to users_path
+  end
+
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,28 @@
+module SessionsHelper
+
+  def current_user=(user)
+    # set the current user object
+    @current_user = user
+  end
+
+  def current_user
+    # find and return the current user object
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def sign_in(user)
+    # handle user login
+    session[:user_id] = user.id
+    self.current_user = user
+  end
+
+  def sign_out
+    session[:user_id] = nil
+    self.current_user = nil
+  end
+
+  def signed_in?
+    ! self.current_user.nil?
+  end
+
+end
