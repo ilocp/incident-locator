fc92b9870493cbe103761f6c25353f800f6a9bb6
Tasos Latsas <tlatsas2000@gmail.com>
2012-08-26 13:22:33 +0300

    Add name field for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
 
-  validates_presence_of :email
+  validates_presence_of :email, :name
   validates_uniqueness_of :email
 
   def self.authenticate(email, password)
