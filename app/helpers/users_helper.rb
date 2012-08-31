dbfbf9fe64407a3c929e486583430280c0beacc6
Tasos Latsas <tlatsas2000@gmail.com>
2012-08-31 13:52:01 +0300

    Add gravatar support in user profiles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+
+  def gravatar_for(user, options = { size: 80, rating: 'g', ssl: true })
+    hash = Digest::MD5::hexdigest(user.email.downcase)
+
+    # build the request url for gravatar
+    if options[:ssl]
+      url = 'https://secure.gravatar.com/'
+    else
+      url = 'http://www.gravatar.com/'
+    end
+    url = "#{url}avatar/#{hash}?s=#{options[:size]}&r#{options[:rating]}"
+
+    image_tag(url, alt: user.name, class: 'gravatar')
+  end
+
 end
