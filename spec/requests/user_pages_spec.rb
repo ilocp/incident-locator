7e7f9527aa5579b513696322938ebadfda98e89d
Tasos Latsas <tlatsas2000@gmail.com>
2012-09-03 14:44:13 +0300

    Tests for signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,6 +10,7 @@ describe "User pages" do
     it { should have_selector('h1', text: 'Sign up') }
   end
 
+
   describe "users page" do
     before { visit users_path }
 
@@ -21,6 +22,30 @@ describe "User pages" do
     before { visit user_path(user) }
 
     it { should have_selector('h1', text: user.name) }
+  end
+
+  describe "signup" do
+    before { visit new_user_path }
+    let (:submit) { "Create new account" }
+
+    describe "with invalid user data" do
+      it "should not create a user" do
+        expect { click_button submit}.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid user data" do
+      before do
+        fill_in "Name", with: "Test user"
+        fill_in "Email", with: "test@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
 end
