72a9c5cdc2593c81da8d1fee93b25cbf8cf28bcb
Tasos Latsas <tlatsas2000@gmail.com>
2012-09-06 14:41:29 +0300

    Refactor signin helper

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,5 +1,6 @@
 # sign in with valid credentials
-def valid_signin(user)
+def signin(user)
+  visit signin_path
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
