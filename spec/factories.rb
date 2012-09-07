1125cdf7a5c0854b10ff294f705b3110237798d8
Tasos Latsas <tlatsas2000@gmail.com>
2012-09-07 12:22:09 +0300

    Allow generating sequence of users with factory girl

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Test User"
-    email    "test@example.com"
+    sequence(:name) { |n| "user#{n}" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
   end
