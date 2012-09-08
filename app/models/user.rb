c77daed674e456b2f29473a9ee0e4ff9b73646da
Tasos Latsas <tlatsas2000@gmail.com>
2012-09-08 13:16:01 +0300

    Use confirmation helper for password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: RE_EMAIL },
             uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
 end
