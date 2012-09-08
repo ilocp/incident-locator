351a915354f5b43af8e328b6ccddf1ab3435ced0
Tasos Latsas <tlatsas2000@gmail.com>
2012-09-08 14:39:24 +0300

    Add report factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,5 +5,12 @@ FactoryGirl.define do
     password "foobar"
     password_confirmation "foobar"
   end
+
+  factory :report do
+    latitude 50.853584
+    longitude 4.362946
+    heading 175
+    user
+  end
 end
 
