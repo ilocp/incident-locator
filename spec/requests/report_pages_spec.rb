243148a90bb7ba8fcbb7e6fe0a9d3070aa099d5e
Tasos Latsas <tlatsas2000@gmail.com>
2012-09-19 12:49:03 +0300

    Test primitive incident creation functionality

--- spec/requests/report_pages_spec.rb
+++ spec/requests/report_pages_spec.rb
@@ -5,11 +5,11 @@ describe "Report pages" do
   subject { page }
 
   let(:user) { FactoryGirl.create(:user) }
+  let(:submit) { 'Create report' }
   before { sign_in user }
 
   describe "report creation" do
     before { visit new_report_path }
-    let(:submit) { 'Create report' }
 
     describe "with invalid data" do
       it "should not create new report" do
@@ -37,6 +37,32 @@ describe "Report pages" do
         before { click_button submit }
 
         it { should have_success_msg('successfully') }
+      end
+    end
+  end
+
+  describe "incident creation" do
+    before { visit new_report_path }
+
+    describe "with two new valid reports on the same area" do
+
+      # 1st report from factory
+      let!(:report1) do
+        FactoryGirl.create(:report, user: user, latitude: 37.980443,
+                           longitude: 23.6691, heading: 90)
+      end
+
+      # second report (through web)
+      before do
+        fill_in "Latitude", with: 37.978802
+        fill_in "Longitude", with: 23.670881
+        fill_in "Heading", with: 65
+      end
+
+      it "should create a new incident with proper ids" do
+        expect { click_button submit }.to change(Report, :count).by(1)
+        incident = Incident.last
+        Report.where(incident_id: incident.id).count.should == 2
       end
     end
   end
