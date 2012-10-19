9d678e15ee2ecd897d149ff31eb9b2f797cf4934
Tasos Latsas <tlatsas2000@gmail.com>
2012-10-19 18:04:21 +0300

    session#create: return json status and message on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,24 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email].downcase)
-    if user && user.authenticate(params[:password])
-      sign_in user
-      redirect_back_or user
-    else
-      flash.now[:error] = 'Invalid email/password'
-      render 'new' #avoid doing a new request with redirect_to
+
+    respond_to do |format|
+      if user && user.authenticate(params[:password])
+        sign_in user
+
+        format.html { redirect_back_or user }
+
+        json_response = { status: 200, msg: 'Authentication successful' }
+        format.json { render json: json_response }
+      else
+        flash.now[:error] = 'Invalid email/password combination'
+
+        #avoid doing a new request with redirect_to
+        format.html { render 'new' }
+
+        json_response = { status: 403, msg: flash.now[:error] }
+        format.json { render json: json_response }
+      end
     end
   end
 
