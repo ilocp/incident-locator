088069f72bcce78b8d69b9077094747b23cc50c8
Tasos Latsas <tlatsas2000@gmail.com>
2012-10-22 12:03:22 +0300

    Api support for creating reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,20 +7,26 @@ class ReportsController < ApplicationController
 
   def create
     @report = current_user.reports.build(params[:report])
-    if @report.save
 
-      # detect possible incidents from this report
-      #
-      # TODO:
-      #   * make it work as a bg job
-      #   * make it more generic and configurable
-      #
-      Geoincident::process(@report)
+    respond_to do |format|
+      if @report.save
 
-      flash[:success] = 'Report created successfully'
-      redirect_to current_user
-    else
-      render 'new'
+        # detect possible incidents from this report
+        #
+        # TODO:
+        #   * make it work as a bg job
+        #   * make it more generic and configurable
+        #
+        Geoincident::process(@report)
+
+        flash[:success] = 'Report created successfully'
+
+        format.html { redirect_to current_user }
+        format.json { render json: { msg: flash[:success] }, status: :ok }
+      else
+        format.html { render 'new' }
+        format.json { render json: { msg: 'Could not save report' }, status: :bad_request }
+      end
     end
   end
 end
