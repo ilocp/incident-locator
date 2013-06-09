347f1dc82dc94c9a813adff75d10fe55c111913a
Tasos Latsas <tlatsas2000@gmail.com>
2013-06-09 13:04:22 +0300

    refactor base.rb to use logger wrapper and separate engine wrappers

--- lib/geoincident/lib/geoincident/base.rb
+++ lib/geoincident/lib/geoincident/base.rb
@@ -4,16 +4,15 @@ module Geoincident
   require 'geoincident/version'
   require 'geoincident/detector'
 
+  extend self
+
   # do we really need this behaviour?
   # maybe use it to get model configuration later (if I get it work)
   #
   #autoload :ActsAsIncidentData, 'geoincident/acts_as_incident_data'
   #autoload :ActsAsIncident,     'geoincident/acts_as_incident'
 
-  def Geoincident::process(report)
-    Geoincident.logger.push_tags(["DETECTION"])
-    Geoincident.logger.info "Incident detection process started on #{Time.now}"
-
+  def run_tracking_incident_engine(report)
     detector = Detector.new
 
     # check if we can attach this report to an existing incident
@@ -29,15 +28,30 @@ module Geoincident
       # belong to the newly-created incident
       unless new_incident.nil?
         detector.scan_reports(new_incident)
+        return new_incident
       end
     end
+
+    existing_incident
+  end
 
   def run_static_incident_engine
     nil
   end
 
+  def with_detector_logger(tags)
+    Geoincident.logger.push_tags(tags)
+    Geoincident.logger.info "Incident detection process started on #{Time.now}"
+    yield
     Geoincident.logger.info "Incident detection process ended on #{Time.now}"
     Geoincident.logger.pop_tags
+  end
+
+  def Geoincident::process(report)
+    log_tags = ["DETECTION"]
+    with_detector_logger log_tags do
+      run_tracking_incident_engine report
+    end
   end
 
 end
